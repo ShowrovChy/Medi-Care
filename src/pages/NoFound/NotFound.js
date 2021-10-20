50d7bbc1c531f6f7dcf2d72929f9f310887f8f39
Showrov <ahshowrov1@gmail.com>
2021-10-20 16:07:33 +0600

    final commit

--- src/pages/NoFound/NotFound.js
+++ src/pages/NoFound/NotFound.js
@@ -1,12 +1,9 @@
 import React from "react";
-
+import errorImg from "../../images/error.png";
 const NotFound = () => {
   return (
-    <div
-      style={{ height: "100vh" }}
-      className="d-flex align-items-center justify-content-center"
-    >
-      <h1 className=" text-danger fw-bolder ">page not Found 404 </h1>
+    <div style={{ height: "100vh" }} className="container">
+      <img className="img-fluid" src={errorImg} alt="" />
     </div>
   );
 };
